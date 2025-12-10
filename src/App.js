458621ed3f664ad5e1cfc29fc34224289e7171ea
GeoCarter96 <geolcarter21@gmail.com>
2025-12-10 13:37:24 -0500

    add

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react' ;
 import './App.css';
 import { auth, db } from './firebase/init';
+import { collection, addDoc, getDocs, getDoc, doc, query, where, updateDoc, deleteDoc } from "firebase/firestore"
 import { createUserWithEmailAndPassword,
 signInWithEmailAndPassword,
 signOut,
@@ -11,6 +12,56 @@ onAuthStateChanged,
 function App() {
   const [user, setUser ] = React.useState({});
   const [loading, setLoading ] = React.useState(true)
+
+  async function updatePost() {
+    const hardcodedId = "AoZkAGkrHoabRgVmX0Uw";
+const postRef = doc(db, "posts", hardcodedId);
+const post = await getPostById(hardcodedId)
+console.log(post)
+const newPost = {
+  ...post,
+  title: "Land $400k Job",
+}
+ updateDoc(postRef, newPost)}
+
+ function deletePost() {
+    const hardcodedId = "AoZkAGkrHoabRgVmX0Uw";
+const postRef = doc(db, "posts", hardcodedId);
+deleteDoc(postRef)
+ }
+
+function createPost() {
+  const post = {
+    title: "Finish Firebase Section",
+    description: "Do Frontend Simplified",
+    uid: user.uid
+  };
+  addDoc(collection(db, "posts"), post)
+} //how to create and post a post
+
+async function getAllPosts() {
+const {docs} = await getDocs(collection(db, "posts"))
+const posts =docs.map(elem => ({...elem.data(), id: elem.id}))
+console.log(posts)
+}//how to add ids to the data in database
+
+async function getPostById(id) {
+
+const postRef = doc(db, "posts", id);
+const postSnap = await getDoc(postRef)
+return postSnap.data()
+console.log(post)
+}//how to recieve a single document
+
+async function getPostByUid() {
+  const postCollectionRef = await query(
+    collection(db, "posts"),
+    where("uid", "==", user.uid)
+  );
+  const { docs } = await getDocs(postCollectionRef);
+  console.log(docs.map(doc => doc.data()))
+}
+
   React.useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       setLoading(false);
@@ -53,6 +104,12 @@ setUser({});
   <button onClick={login}>Login</button>
   <button onClick={logout}>Logout</button>
   {loading ? 'loading....' : user.email}
+  <button onClick={createPost}>Create Post</button>
+  <button onClick={getAllPosts}>Get All Posts</button>
+   <button onClick={getPostById}>Get Post By Id</button>
+   <button onClick={getPostByUid}>Get Post By Uid</button>
+   <button onClick={updatePost}>Update Post</button>
+   <button onClick={deletePost}>Delete Post</button>
     </div>
   );
 }
